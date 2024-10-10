2fc75bcc4b451ab3ec255ec8dc47620864988e5b
sergioismaelvc <sirsergioismael@gmail.com>
2024-10-10 12:12:37 -0300

    black mode

--- script.js
+++ script.js
@@ -236,3 +236,26 @@ document.querySelector('.new-poem').addEventListener('mouseleave', function() {
   const container = this.nextElementSibling;
   container.style.display = "none";
 });
+
+
+//darkmode whitemode
+const toggleSwitch = document.querySelector('#theme-toggle');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+  }
+}
+
+toggleSwitch.addEventListener('change', function () {
+  if (this.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+  }
+});
